33a0d1bbe8f85231761aa79eb58cc08945589db1
vanea36070 <40001664+vanea36070@users.noreply.github.com>
2018-06-06 20:28:13 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,9 @@
+ var x = 4;
+ var y = 5;
+ if (x < y) {
+    document.write("x is less than y");
+ } else if (x > y); {
+    document.write("x is greter than y"); 
+ } else (x = y); {
+    document.write("x equals y");
+ }
---
+++ task2.js
@@ -0,0 +1,4 @@
+var fruitList = new Array("appel", "watermelon", "lemon", "orange", "kiwi");
+for (var i = 0; i < fruitList.length; i++) {
+	document.write(fruitList[i] + "<br />");
+}
