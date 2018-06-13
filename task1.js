cbe40e8f4c646667d8daa729a7ffa254c5a52863
vanea36070 <40001664+vanea36070@users.noreply.github.com>
2018-06-13 11:57:11 +0300

    Delete task1.js

--- task1.js
+++
@@ -1,9 +0,0 @@
- var x = 4;
- var y = 5;
- if (x < y) {
-    document.write("x is less than y");
- } else if (x > y) {
-    document.write("x is greter than y"); 
- } else if (x == y) {
-    document.write("x equals y");
- }
